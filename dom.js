c2ebe0e2ece867b814cb24560f877e7f67e967e8
husseiyare12@gmail.com <husseiyare12@gmail.com>
2025-12-12 19:26:56 +0300

    ibdex.html

---
+++ dom.js
@@ -0,0 +1,44 @@
+// let hello =document.getElementById("first")
+// hello.style.color= "yellow"
+// hello.style.backgroundColor="green"
+// hello.style.width="500px"
+
+
+// const hero=document.getElementsByClassName("hero")
+// hero[0].style.backgroundColor="blue"
+// hero[0].style.width="200px"
+// hero[0].style.height="200px"
+// hero[0].style.borderRadius="50%"
+
+
+
+
+// const first = document.querySelector("#first")
+
+// const text = document.querySelector(".text")
+
+// first.style.color= "green"
+
+// text.style.backgroundColor  = "orange"
+
+// // next=>
+
+// const bnt=document.querySelector("button")
+// bnt.addEventListener("click", ()=>{
+// document.body.style.backgroundColor="green"
+// });
+
+// const bnt2=document.querySelector("#bnt2")
+// bnt2.addEventListener("click", ()=>{
+// document.body.style.backgroundColor="white"
+// })
+
+
+
+btn3.two=document,addEventListener("click",()=>{
+box.style.display="block"
+})
+
+
+
+
